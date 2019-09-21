e64241bc5bc935aab2baee513a8729326a95ea06
Loliud <anhhaiquynhon123@gmail.com>
2019-09-21 16:28:02 +0700

    home template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,18 +6,7 @@ class  App extends Component {
  render(){
    return (
      <div>
-       <div id="menu">
-         <div className="overlay">
-         <h1>Coders.thoidai</h1>
-         <ul id="list">
-           <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png"/></span>Go training</li>
-           <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png"/></span>Guide</li>
-           <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png"/></span>Contact</li>
-           {/* <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png"/></span>Contact</li> */}
-           {/* <li className="item"><span><i className="fas fa-arrow-right"></i></span>Contact</li> */}
-         </ul>
-         </div>
-       </div>
+      
        <Home/>
      </div>
    )
--- my-app/src/components/home/home.js
+++ my-app/src/components/home/home.js
@@ -1,17 +1,33 @@
-import React, {Component} from 'react';
-
-class Home extends Component{
-    constructor(props){
+import React, { Component } from 'react';
+import './home.css';
+class Home extends Component {
+    constructor(props) {
         super(props)
-        this.state ={
+        this.state = {
 
         }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                
+            <div id="menu">
+                <div className="overlay">
+                    <h1>CoderS nihonGo</h1>
+                    <ul id="list">
+                        <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png" alt="sword"/></span>Go training</li>
+                        <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png" alt="sword"/></span>Guide</li>
+                        <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png" alt="sword"/></span>Contact</li>
+                        {/* <li className="item"><span><img src="http://pngimg.com/uploads/katana/katana_PNG14.png"/></span>Contact</li> */}
+                        {/* <li className="item"><span><i className="fas fa-arrow-right"></i></span>Contact</li> */}
+                    </ul>
+                </div>
+              
+            </div>
+            <div id="water">
+            <div class="animate drop"></div>
+            <div class="animate wave"></div>
+            </div>
             </div>
         )
     }
